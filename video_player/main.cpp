1661c74ddbf703d910b83c18086ce1630f2aaccd
wbxie <null>
2023-07-09 19:49:09 +0800

    Initial Commit

---
+++ video_player/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  video_player
+//
+//  Created by wbxie on 2023/7/9.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
